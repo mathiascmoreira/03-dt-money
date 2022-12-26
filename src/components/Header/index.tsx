8761c8a1e07ae63b638c08fbeeebb1c41726f619
Mathias <mathiascmoreira@Gmail.com>
2022-12-25 22:27:36 -0700

    06

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -21,8 +21,6 @@ export function Header() {
                     </Dialog.Content>
                 </Dialog.Portal>
                 </Dialog.Root>
-
-               
             </HeaderContent>
         </HeaderContainer>
     )
