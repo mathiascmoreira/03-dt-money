484252660b3aa00c7522489d89bda376f302d68d
Mathias <mathiascmoreira@Gmail.com>
2022-12-27 16:33:44 -0700

    21

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,34 +1,34 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
-import { useSummary } from '../../hooks/useSummary';
-import { priceFormatter } from '../../util/ formatter';
-import { SummaryCard, SummaryContainer } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useSummary } from '../../hooks/useSummary'
+import { priceFormatter } from '../../util/ formatter'
+import { SummaryCard, SummaryContainer } from './styles'
 
 export function Summary() {
-    const summary = useSummary();
+  const summary = useSummary()
 
-    return (
-        <SummaryContainer>
-            <SummaryCard>
-                <header>
-                    <span>Entradas</span>
-                    <ArrowCircleUp size={32} color="#00b37e" />
-                </header>
-                <strong>{priceFormatter.format(summary.income)}</strong>
-            </SummaryCard>
-            <SummaryCard>
-                <header>
-                    <span>Saída</span>
-                    <ArrowCircleDown size={32} color="#f75a68" />
-                </header>
-                <strong>{priceFormatter.format(summary.outcome)}</strong>
-            </SummaryCard>
-            <SummaryCard variant='green'>
-                <header>
-                    <span>Entradas</span>
-                    <CurrencyDollar size={32} color="#fff" />
-                </header>
-                <strong>{priceFormatter.format(summary.total)}</strong>
-            </SummaryCard>
-        </SummaryContainer>
-    )
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+        <strong>{priceFormatter.format(summary.income)}</strong>
+      </SummaryCard>
+      <SummaryCard>
+        <header>
+          <span>Saída</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <span>Entradas</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+        <strong>{priceFormatter.format(summary.total)}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
 }
